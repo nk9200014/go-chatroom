fad6268ddd117228094c75addf5aac7185b8367d
nk9200014 <nk9200014@gmail.com>
2023-05-30 05:23:07 +0800

    fix function removeConn

--- chatroom_server.go
+++ chatroom_server.go
@@ -41,22 +41,22 @@ func (c *connPool) execute() {
 	// Infinite loop to catch register and unregister event.
 	for {
 		select {
-		// Add websocket connection to the pool when catch register event.
+		// Add WebSocket connection to the pool when catch register event.
 		case r := <-c.register:
 			c.connections = append(c.connections, r)
-			log.Println("Websocket connected,", r.Request().RemoteAddr, "register.")
-			log.Println("Current connection pool:", c.getPoolAddr())
-		// Remove websocket connection from the pool when catch unregister event.
+			log.Println("WebSocket connected,", r.Request().RemoteAddr, "register.")
+			log.Println("Current connection pool:", c.GetPoolAddr())
+		// Remove WebSocket connection from the pool when catch unregister event.
 		case r := <-c.unregister:
 			c.connections = removeConn(c.connections, r)
-			log.Println("Websocket disconnected,", r.Request().RemoteAddr, "unregister.")
-			log.Println("Current connection pool:", c.getPoolAddr())
+			log.Println("WebSocket disconnected,", r.Request().RemoteAddr, "unregister.")
+			log.Println("Current connection pool:", c.GetPoolAddr())
 		}
 	}
 }
 
 // Retrieves all IP addresses of the connections in connPool.
-func (c *connPool) getPoolAddr() []string {
+func (c *connPool) GetPoolAddr() []string {
 	var slice []string
 	for _, ws := range c.connections {
 		slice = append(slice, ws.Request().RemoteAddr)
@@ -64,10 +64,16 @@ func (c *connPool) getPoolAddr() []string {
 	return slice
 }
 
-// Removes the websocket connection elem from the slice and returns the modified slice.
+// Removes the WebSocket connection elem from the slice and returns the modified slice.
 // If elem does not exist in the slice, returns the original unchanged slice.
 func removeConn(slice []*websocket.Conn, elem *websocket.Conn) []*websocket.Conn {
-	newSlice := make([]*websocket.Conn, len(slice)-1)
+	var newSliceLen int
+	if len(slice) <= 0 {
+		newSliceLen = 0
+	} else {
+		newSliceLen = len(slice) - 1
+	}
+	newSlice := make([]*websocket.Conn, newSliceLen)
 	for i, origElem := range slice {
 		if origElem == elem {
 			newSlice = append(slice[:i], slice[i+1:]...)
@@ -79,18 +85,18 @@ func removeConn(slice []*websocket.Conn, elem *websocket.Conn) []*websocket.Conn
 
 // When establishing a WebSocket connection, the server verifies the password and registers the client.
 // If the password is incorrect, the registration process will be canceled and returned an error message to client.
-func (c *ChatServer) registerServer(ws *websocket.Conn) {
-	// Close websocket connextion before return.
+func (s *ChatServer) registerServer(ws *websocket.Conn) {
+	// Close WebSocket connextion before return.
 	defer ws.Close()
 	// Get chatroom password parameter form url.
 	params := ws.Request().URL.Query()
 	password := params.Get("pwd")
 	// Check the password is correct or not,
 	// if the chat server is public, skip password checking.
-	if c.password == "" || c.password == password {
+	if s.password == "" || s.password == password {
 		// Register the connection to the ConnPool and continue listening.
-		c.serverConnPool.register <- ws
-		c.readMessage(ws)
+		s.serverConnPool.register <- ws
+		s.readMessage(ws)
 	} else {
 		log.Println(ws.Request().RemoteAddr, "Client connection failed: Incorrect password.")
 		// TODO: send error message to client
@@ -99,26 +105,26 @@ func (c *ChatServer) registerServer(ws *websocket.Conn) {
 
 // A blocking function that continues listening for WebSocket messages.
 // If the connection is disconnected, it should be unregistered from the ConnPool.
-func (c *ChatServer) readMessage(ws *websocket.Conn) {
+func (s *ChatServer) readMessage(ws *websocket.Conn) {
 	var message string
 	for {
 		err := websocket.Message.Receive(ws, &message)
 		if err != nil {
-			c.serverConnPool.unregister <- ws
+			s.serverConnPool.unregister <- ws
 			log.Println(err)
 			return
 		}
 		log.Println(ws.Request().RemoteAddr, ":", message)
-		c.broadcast(message)
+		s.Broadcast(message)
 	}
 }
 
 // Broadcast the message on the chat server ConnPool.
-func (c *ChatServer) broadcast(message string) (err error) {
-	for _, ws := range c.serverConnPool.connections {
+func (s *ChatServer) Broadcast(message string) (err error) {
+	for _, ws := range s.serverConnPool.connections {
 		if err := websocket.Message.Send(ws, message); err != nil {
 			// Remove the connection from ConnPool
-			c.serverConnPool.unregister <- ws
+			s.serverConnPool.unregister <- ws
 			log.Println(ws.Request().RemoteAddr, "disconnected :", err)
 			return err
 		}
@@ -127,13 +133,13 @@ func (c *ChatServer) broadcast(message string) (err error) {
 }
 
 // A blocking function that run the chat server.
-func (c *ChatServer) Run() {
+func (s *ChatServer) Run() {
 	// Listing ConnPool.
-	go c.serverConnPool.execute()
+	go s.serverConnPool.execute()
 	// TODO: Maybe support "/register" to a custom setting.
-	// websocket handling.
-	http.Handle("/register", websocket.Handler(c.registerServer))
-	err := http.ListenAndServe(c.listenAddr, nil)
+	// WebSocket handling.
+	http.Handle("/register", websocket.Handler(s.registerServer))
+	err := http.ListenAndServe(s.listenAddr, nil)
 	if err != nil {
 		log.Panic("ListenAndServe: " + err.Error())
 	}
